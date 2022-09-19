8aa86a4773080cfcbda6f20f5f359e3766a68e0d
uribo <ur1b0.dev@gmail.com>
2022-09-19 22:48:19 +0900

    コメントを追加

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ old = {}
 
 def check_client():
   s = ""
+  # 増えたクライアントを確認
   ret = {i:new[i] for i in new.keys() if i not in old.keys()}
   if 0 != len(ret):
     s = s+f"join clients"
     for i in ret.keys():
       s = s+f"\n{i}:{ret[i]}"
 
+  # 減ったクライアントを確認
   ret = {i:old[i] for i in old.keys() if i not in new.keys()}
   if 0 != len(ret):
     s = s+f"\n\nleave clients"
