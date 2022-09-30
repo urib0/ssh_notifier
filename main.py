df618d8268da2298cbba3d183691226eb8c4f0e9
uribo <ur1b0.dev@gmail.com>
2022-09-30 17:54:39 +0900

    モジュール化した

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import json
 import requests
 
-def check_client():
+def check_client(new,old):
   s = ""
   # 増えたクライアントを確認
   ret = {i:new[i] for i in new.keys() if i not in old.keys()}
@@ -30,26 +30,31 @@ def send_message(token,message):
         "message": (None, message),
     }
     res = requests.post("https://notify-api.line.me/api/notify", headers=headers, files=files)
+    return res
 
-old = {}
-while(True):
-  # 設定読み込み
-  with open("./config.json", "r") as f:
-    conf = json.loads(f.read())
+def main():
+  old = {}
+  while(True):
+    # 設定読み込み
+    with open("./config.json", "r") as f:
+      conf = json.loads(f.read())
 
-  new = ssh_client_view.main()
-  ret = check_client()
+    new = ssh_client_view.main()
+    ret = check_client(new,old)
 
-  # 増減があれば現在のクライアントを追加
-  if 0 != len(ret):
-    ret = ret+f"\n- current clients\n"
-    for i in new.keys():
-      ret = ret+f"{i}:{new[i]}\n"
-    ret = ret+"\n"
-    print(ret)
+    # 増減があれば現在のクライアントを追加
+    if 0 != len(ret):
+      ret = ret+f"\n- current clients\n"
+      for i in new.keys():
+        ret = ret+f"{i}:{new[i]}\n"
+      ret = ret+"\n"
+      print(ret)
 
-    # line送信
-    send_message(conf["line_token"],ret)
+      # line送信
+      send_message(conf["line_token"],ret)
 
-  old = new
-  time.sleep(conf["interval"])
+    old = new
+    time.sleep(conf["interval"])
+
+if __name__ == "__main__":
+  main()
